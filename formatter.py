051c38f530cf06c6db22b7d7c24e8be404fd3c91
Jesse Whitehouse <jesse@whitehouse.dev>
2020-03-22 10:58:34 -0500

    Moves deepcopy into wrapper.

--- formatter.py
+++ formatter.py
@@ -7,20 +7,21 @@ def fill_parameters(txt:str, params:dict) -> str:
 
 	if not all([i in get_parameter_names(txt) for i in params]):
 		raise BindingException("Parameter input was not valid.")
+
+	cpy_params = copy.deepcopy(params)
 	
 	def recursive_replace(params:dict, txt:str):
 
-		cpy_params = copy.deepcopy(params)
-		key = cpy_params.__iter__().__next__()
-		new_str = txt.replace(':%s' % key, "'%s'" % cpy_params.get(key))
-		cpy_params.pop(key)
+		key = params.__iter__().__next__()
+		new_str = txt.replace(':%s' % key, "'%s'" % params.get(key))
+		params.pop(key)
 
-		if len(cpy_params) == 0:
+		if len(params) == 0:
 			return new_str
 		else:
-			return recursive_replace(cpy_params, new_str)
+			return recursive_replace(params, new_str)
 
-	return recursive_replace(params, txt)
+	return recursive_replace(cpy_params, txt)
 
 
 def get_parameter_names(txt:str) -> list:
